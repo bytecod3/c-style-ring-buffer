6a71dce3fc889cfb3ce3087a562f97d2b534f06d
bytecod3 <emwiti658@gmail.com>
2025-11-18 18:22:56 +0300

    ADDS c-style buffer

---
+++ ring_buffer.c
@@ -0,0 +1,123 @@
+/**
+ * @file ring_buffer.cpp
+ * @brief This file implements functions for ring buffer for data acquisition
+ * @author Edwin Mwiti
+ */
+
+#include "ring_buffer.h"
+
+/**
+ * @brief Initialize the ring buffer
+ * @return 
+ */
+ring_buffer_t ring_buffer_init() {
+    ring_buffer_t mem = (ring_buffer_t)malloc(sizeof(struct ring_buffer));
+
+    if(mem != NULL) {
+        mem->head = 0;
+        mem->tail = 0;
+        memset(mem->data, 0, RING_BUFFER_LENGTH);
+    } else {
+        return NULL;
+    }
+
+    return mem;
+}
+
+/**
+ * @brief checks if the ring buffer is empty
+ */
+uint8_t ring_buffer_empty(ring_buffer_t buffer)
+{
+    if(buffer->head == buffer->tail) {
+        return 1;
+    } else {
+        return ERR_EMPTY_FULL;
+    }
+}
+
+/**
+ * @brief Checks if the ring buffer is full
+ */
+uint8_t ring_buffer_full(ring_buffer_t buffer)
+{
+    if(buffer->head == RING_BUFFER_LENGTH) {
+        return 1;
+    } else {
+        return ERR_EMPTY_FULL;
+    }
+}
+
+/**
+ * @brief Add an item to the buffer 
+ * @param buffer pointer to the buffer location
+ * @param item uint16_t item to add
+ */
+void ring_buffer_add(ring_buffer_t buffer, uint16_t item)
+{
+
+    if(buffer->data != NULL) {
+        if(ring_buffer_full(buffer)) {
+            buffer->head = buffer->head % RING_BUFFER_LENGTH;
+        }   
+        buffer->data[buffer->head++] = item;
+    }
+}
+
+/**
+ * @brief Remove an item from the tail of the buffer 
+ * @param buffer buffer to remove the item from
+ */
+uint16_t ring_buffer_remove(ring_buffer_t buffer) {
+    if(buffer != NULL) {
+        uint16_t a = buffer->data[buffer->tail];
+        buffer->tail += 1;
+        return a;
+    } else {
+        return ERR_EMPTY_FULL;
+    }
+}
+
+/**
+ * @brief Set all values of the buffer to 0
+ */
+void ring_buffer_purge(ring_buffer_t buffer) {
+    puts("\r\npurging buffer...");
+    if(check_not_null(buffer)) {
+        for(int i = 0; i < RING_BUFFER_LENGTH; i++) {
+            buffer->data[i] = 0;
+        }
+    }
+}
+
+/**
+ * @brief Check that the pointer to buffer passed is not NULL
+ * @param b pointer to check
+ */
+uint8_t check_not_null(ring_buffer_t buffer) {
+    return (buffer != NULL);
+}
+
+/**
+ * @brief Print all the contents of the buffer
+ * @param buffer ring buffer instance
+ */
+void dump_ring_buffer(ring_buffer_t buffer)
+{
+    if(!ring_buffer_empty(buffer)) {
+        for(uint8_t i = 0; i < RING_BUFFER_LENGTH; i++) {
+
+            #if (DUMP_VERTICAL ^ DUMP_LINEAR)
+                #if DUMP_VERTICAL
+                            printf("indx: [%d], value: %d\r\n", i, buffer->data[i]);
+                #elif DUMP_LINEAR
+                            printf("%d ", buffer->data[i]);
+                #endif
+            #endif
+        }
+
+
+    } else {
+        puts("Buffer is empty");
+    }
+}
---
+++ ring_buffer.h
@@ -0,0 +1,46 @@
+/**
+ * @file ring_buffer.h 
+ * @brief Declares functions to use to implement a ring buffer for data filtering 
+ * @author Edwin Mwiti
+ */
+
+#pragma once 
+
+#include <iostream>
+#include <cstdint>
+#include <cstring>
+#include <cstdlib>
+
+#define DUMP_LINEAR     (1)  ///< print array in a straight line 
+#define DUMP_VERTICAL   (0)  ///< printf array in vertical fashion showing index and value
+
+/**
+ * Errors list for ring buffer functions 
+ */
+typedef enum
+{
+    ERR_EMPTY_FULL = 0,
+    ERR_NULL = -1,
+    ERR_FULL = -2,
+    ERR_FATAL = -3
+} ERR;
+
+#define  RING_BUFFER_LENGTH   (10)       ///< how many items the buffer can hold 
+
+
+struct ring_buffer {
+    uint8_t head;
+    uint8_t tail;
+    uint16_t data[RING_BUFFER_LENGTH];
+};
+
+typedef struct ring_buffer* ring_buffer_t;
+
+ring_buffer_t ring_buffer_init();
+uint8_t ring_buffer_empty(ring_buffer_t buffer);
+uint8_t ring_buffer_full(ring_buffer_t buffer);
+void ring_buffer_add(ring_buffer_t buffer, uint16_t item);
+void ring_buffer_remove();
+void ring_buffer_purge(ring_buffer_t);
+uint8_t check_not_null(ring_buffer_t);
+void dump_ring_buffer(ring_buffer_t buffer);
